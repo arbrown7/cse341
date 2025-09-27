2d9be883caecab384abb103780e930f9a1e90dfd
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2025-09-27 14:27:27 -0600

    w02 personal assignment

--- routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getHome } = require('../controllers/homeController');
-
-// When the user visits "/", call getHome
-router.get('/', getHome);
-
-module.exports = router;
