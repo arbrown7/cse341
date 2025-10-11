112f9510f7bf2040d9620190d69a4ce8a7f0b2d6
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2025-10-10 22:19:47 -0600

    personal assignment

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger.json');
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -9,9 +11,15 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
     next();
   })
-  .use('/', require('./routes'));
+  .use('/', require('./routes'))
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
